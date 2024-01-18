b131d619bdb035b16b1f9cdf489daa5a810ba5b1
Patryk Stępień <fighting.dreamer.0@gmail.com>
2024-01-18 23:02:55 +0100

    Rename mutations

--- flyps/user/query.py
+++ flyps/user/query.py
@@ -7,9 +7,9 @@ from flyps.user.types import User, Users
 @strawberry.type
 class UserQuery:
     @strawberry.field
-    async def all(self) -> Users:
+    async def get_all(self) -> Users:
         return Users(users=await get_all("user"))
 
     @strawberry.mutation
-    async def get(self, id: strawberry.ID) -> User:
+    async def get_one(self, id: strawberry.ID) -> User:
         return await get_one("user", id)
