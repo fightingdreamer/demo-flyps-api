6a9019cb4bfc5774c1c0422d9ef387a6bb59f37a
Patryk Stępień <fighting.dreamer.0@gmail.com>
2024-01-19 03:09:57 +0100

    Add dev/prd

--- flyps/api.py
+++ flyps/api.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 from strawberry.fastapi import GraphQLRouter
 
@@ -7,3 +8,26 @@ graphql_router = GraphQLRouter(schema)
 
 app = FastAPI()
 app.include_router(graphql_router, prefix="/graphql")
+
+
+def dev():
+    uvicorn.run(
+        "flyps.api:app",
+        host="127.0.0.1",
+        port=5000,
+        log_level="info",
+        reload=True,
+        reload_delay=2,
+        workers=1,
+    )
+
+
+def prd():
+    uvicorn.run(
+        "flyps.api:app",
+        host="0.0.0.0",
+        port=5000,
+        log_level="info",
+        reload=False,
+        workers=4,
+    )
